885a77cca9d70243c2f9fb175893a980e8c360c0
céline leguelvouit <celin_tfx35@MacBook-Pro.local>
2024-11-10 17:52:44 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("document loaded 🥹");
+
+  const buttonContact = document.querySelector(".connect");
+  buttonContact.addEventListener("click", () => {
+    console.log("cliqué");
+    document.querySelector(".modal").classList.add("hide");
+
+    const closeForm = document.querySelector(".close");
+    closeForm.addEventListener("click", () => {
+      console.log("cliqué");
+      document.querySelector(".modal").classList.remove("hide");
+    });
+    document.querySelector(".submit").addEventListener("click", () => {
+      document.querySelector(".modal").classList.remove("hide");
+    });
+  });
+  document.querySelector(".submit").addEventListener("click", () => {
+    document
+      .querySelector("#a-awesome-form")
+      .addEventListener("submit", async (e) => {
+        try {
+          const data = {
+            firstname: document.querySelector("#firstname").value,
+            lastname: document.querySelector("#lastname").value,
+            email: document.querySelector("#email").value,
+            message: document.querySelector("#message").value,
+          };
+          if (!data.email) {
+            document.querySelector(".modal").classList.remove("hide");
+          } else {
+            if (data.email) console.log(data);
+            const response = await axios.post(
+              "https://site--backend-form-tripadvisor--dm4qbjsg7dww.code.run/form",
+              data
+            );
+            console.log(response);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      });
+  });
+
+  window.addEventListener("scroll", () => {
+    document.querySelector("header").classList.add("scrollHide");
+    if (window.scrollY === 0) {
+      document.querySelector("header").classList.remove("scrollHide");
+    }
+  });
+
+  document.querySelector(".magic").addEventListener("click", () => {
+    document.querySelector(".magic").classList.toggle("img");
+  });
+});
